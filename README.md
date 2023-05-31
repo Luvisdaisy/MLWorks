dfa9164051b61b1d0185d3fbdc4844dc84022d03
Tian Haotian <qi_7ran@163.com>
2023-05-31 13:22:34 +0800

    准备使用mongodb存储数据

--- README.md
+++ README.md
@@ -2,5 +2,6 @@
 The works done while learning ML
 # 2023.5.31
 信息爬虫正常，代理仍然不稳定，被反扒404
+准备使用mongodb存储数据，test.py中已测试成功
 # 2023.5.30
 爬虫部分待测试，解决代理问题
--- test.py
+++ test.py
@@ -1,4 +1,41 @@
-count = 0
-while count < 25:
-    count += 1
-    print(count)
+import csv
+from pymongo import MongoClient
+
+movie_list = []
+
+# 读取CSV文件数据
+with open('E:\ProSpace\VSCodePros\Python\MLFinalWork\movie_data.csv', 'r', encoding='utf-8') as f:
+    reader = csv.reader(f)
+    movie_list = list(reader)
+
+# 连接到MongoDB服务器
+client = MongoClient('mongodb://localhost:27017/')
+
+# 选择或创建数据库
+db = client['mytestdatabase']
+
+# 选择或创建集合
+collection = db['mytestcollection']
+
+# 遍历电影列表并存储到MongoDB
+for movie in movie_list:
+    movie_data = {
+        'num': movie[0],
+        'id': movie[1],
+        'year': movie[2],
+        'title': movie[3],
+        'director': movie[4],
+        'actors': movie[5],
+        'type': movie[6],
+        'country': movie[7],
+        'duration': movie[8]
+    }
+    collection.insert_one(movie_data)
+
+# 遍历集合中的数据
+for movie_data in collection.find():
+    # 处理每个电影数据
+    print(movie_data)
+
+# 断开与MongoDB服务器的连接
+client.close()
